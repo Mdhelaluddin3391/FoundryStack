976c000d5a31c3e8b14c9e29b57e1ba561e8fdcb
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2025-12-20 04:46:45 +0530

    resoved a small error

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// Empty file to test if the presence of main.js fixes the scrolling issue.
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.overflow = "auto";
+});
